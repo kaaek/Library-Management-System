b5d07e67108d4497e9183f2398022a872e3dabe2
Khalil El Kaaki <khalil.el.kaaki@gmail.com>
2025-08-18 10:08:16 +0300

    fetchBook() functional

--- src/main/java/com/example/lms/dto/openLibrary/OpenLibraryResponseDTO.java
+++ src/main/java/com/example/lms/dto/openLibrary/OpenLibraryResponseDTO.java
@@ -1,10 +1,14 @@
 package com.example.lms.dto.openLibrary;
 
-import java.util.Map;
+import java.util.LinkedHashMap;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OpenLibraryResponseDTO {
-    private Map<String, BookApiResponseDTO> books; // need this structure since Open Library structures its JSON by keying it by ISBN
+    private LinkedHashMap<String, BookApiResponseDTO> books; // need this structure since Open Library structures its JSON by keying it by ISBN
 }
--- src/main/java/com/example/lms/service/OpenLibraryApiService.java
+++ src/main/java/com/example/lms/service/OpenLibraryApiService.java
@@ -1,21 +1,42 @@
 package com.example.lms.service;
 
-import java.util.Map;
-
+import java.util.LinkedHashMap;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.example.lms.dto.openLibrary.BookApiResponseDTO;
-import com.example.lms.dto.openLibrary.OpenLibraryResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class OpenLibraryApiService {
     
+    private final ObjectMapper objectMapper;
     private final RestTemplate restTemplate;
 
-    public OpenLibraryApiService(RestTemplate restTemplate) {
+    public OpenLibraryApiService(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public BookApiResponseDTO fetchBook(String isbn) {
+        String apiUrl = "https://openlibrary.org/api/books?bibkeys=ISBN:"+isbn+"&format=json&jscmd=data";
+        try {
+            LinkedHashMap<String, Object> response = restTemplate.getForObject(apiUrl, LinkedHashMap.class);
+            if (response == null || response.isEmpty() || response.get("ISBN:" + isbn) == null) {
+                throw new IllegalArgumentException("Query for ISBN: " + isbn + " returned a null response.");
+            }
+            BookApiResponseDTO book = objectMapper.convertValue(response.get("ISBN:" + isbn), BookApiResponseDTO.class);
+
+            if (book == null) {
+                throw new EntityNotFoundException("Open Library returned a non-null response with a null book entry."); // safely handle missing ISBN key
+            }
+            
+            return book;
+        
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Query for ISBN: " + isbn + " returned a null response.");
+        }
     }
 
     // public String fetchAuthorName(String isbn) {
@@ -37,28 +58,5 @@ public class OpenLibraryApiService {
     //         return "Unknown Author"; // fallback on any request/parse error
     //     }
     // }
-
-
-    public BookApiResponseDTO fetchBook(String isbn) {
-        String apiUrl = "https://openlibrary.org/api/books?bibkeys=ISBN:"+isbn+"&format=json&jscmd=data";
-        try {
-            OpenLibraryResponseDTO response = restTemplate.getForObject(apiUrl, OpenLibraryResponseDTO.class);
-            if (response == null || response.getBooks() == null) {
-            // return null; // API returned nothing or invalid structure
-            throw new IllegalArgumentException("Query for ISBN: " + isbn + " returned a null response.");
-            }
-            BookApiResponseDTO book = response.getBooks().get("ISBN:" + isbn);
-
-            if (book == null) {
-                throw new EntityNotFoundException("Open Library returned a non-null response with a null book entry."); // safely handle missing ISBN key
-            }
-            
-            return book;
-        
-        } catch (Exception e) {
-            // return null; // fallback on any request/parse error
-            throw new IllegalArgumentException("Query for ISBN: " + isbn + " returned a null response.");
-        }
-    }
 
 }
